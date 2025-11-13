a77172f5c91a5e4cc04d0c32dec5607ab31decaa
Nathan LECLER <nthn.lclr@gmail.com>
2025-11-13 15:36:15 +0100

    minor UI fix

--- src/components/TestUrssaf.jsx
+++ src/components/TestUrssaf.jsx
@@ -8,6 +8,7 @@ function TestUrssaf() {
   const [brut, setBrut] = useState();
   const [net, setNet] = useState();
   const [cotis, setCotis] = useState();
+  const [prime, setPrime] = useState();
 
   const handleNumberInput = (setter) => (e) => {
     // Autorise uniquement chiffres + virgule ou point
@@ -19,6 +20,7 @@ function TestUrssaf() {
     if (!taux || !hours) return alert("Veuillez remplir tous les champs.");
     // Changer la valeur ci-dessous pour voir le net s'actualiser
     const brutValue = `${taux * hours} €/mois`;
+    const primeValue = `${prime} €/mois`;
     setBrut(brutValue); // le brut à calculer en €/mois
     console.log("Son brut est de ", brut);
     const engine = new Engine(rules);
@@ -29,23 +31,30 @@ function TestUrssaf() {
           "salarié . contrat": "'CDI'",
           "salarié . contrat . statut cadre": "non",
           "salarié . activité partielle": "oui",
-          "salarié . rémunération . primes . activité": "104 €/mois",
+          "salarié . rémunération . primes . activité": primeValue,
           "salarié . contrat . temps de travail . temps partiel": "oui",
           "salarié . convention collective": "'HCR'",
           "établissement . commune . code postal": "31000",
-          "établissement . taux ATMP . taux collectif": "2.12 %",
+          "établissement . taux ATMP": "1.77 %",
+          //Pas sur de celui ci dessous:
+          "salarié . régimes spécifiques . taux réduits . réduction de taux . cotisations maladie employeur taux":
+            "2.12",
           "entreprise . salariés . effectif . seuil . moins de 11": "oui",
         })
         .evaluate("salarié . rémunération . net . à payer avant impôt")
     );
     setCotis(engine.evaluate("salarié . coût total employeur"));
+    console.log(
+      "Salarié rémunération brut",
+      engine.evaluate("salarié . rémunération . brut")
+    );
     console.log(engine.evaluate("salarié . coût total employeur"));
   };
 
   return (
     <div className="bg-white shadow-2xl rounded-2xl p-8 max-w-lg w-full border border-gray-100">
       <h1 className="text-3xl font-bold text-center text-indigo-700 mb-6">
-        🧮 Simulateur URSSAF
+        Simulateur URSSAF
       </h1>
 
       <div className="flex flex-col gap-4">
@@ -66,7 +75,15 @@ function TestUrssaf() {
           value={taux}
           onChange={handleNumberInput(setTaux)}
         />
-
+        <label className="font-semibold text-gray-700">
+          Prime d'activité (€)
+        </label>
+        <input
+          className="bg-gray-50 border border-gray-200 rounded-lg px-4 py-2 text-lg focus:outline-none focus:ring-2 focus:ring-indigo-400"
+          placeholder="Ex : 100"
+          value={prime}
+          onChange={handleNumberInput(setPrime)}
+        />
         <button
           onClick={handleClick}
           className="mt-4 bg-indigo-600 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded-lg transition duration-300"
@@ -80,11 +97,11 @@ function TestUrssaf() {
           <p className="text-gray-700 text-lg">
             💰 <strong>Salaire brut :</strong> {brut}
           </p>
-          <p className="text-green-700 text-lg mt-2">
+          <p className="text-gray-700 text-lg mt-2">
             🧾 <strong>Salaire net à payer avant impots:</strong>{" "}
             {formatValue(net)}
           </p>
-          <p className="text-red-700 text-lg mt-2">
+          <p className="text-gray-700 text-lg mt-2">
             🏢 <strong>Cotisations employeur :</strong> {formatValue(cotis)}
           </p>
         </div>
